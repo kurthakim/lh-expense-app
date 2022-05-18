3e2eeb95d7f30e7325df61adc63883ec19293fcc
Kurt Hakim <kurthakim@gmail.com>
2022-05-18 15:59:38 +0700

    Initial commit

---
+++ src/app.controller.ts
@@ -0,0 +1,32 @@
+import { Controller, Get, Post, Put, Delete } from '@nestjs/common';
+import { AppService } from './app.service';
+
+@Controller('report/:type')
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+
+  @Get()
+  getAllReports() {
+    return [];
+  }
+
+  @Get(':id')
+  getReportById() {
+    return {};
+  }
+
+  @Post()
+  createReport() {
+    return 'Created';
+  }
+
+  @Put(':id')
+  updateReport() {
+    return 'Update';
+  }
+
+  @Delete(':id')
+  deleteReport() {
+    return 'delete';
+  }
+}
